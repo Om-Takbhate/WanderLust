8c924c42f4c9f9a42c9da83d233e24a45afed9db
Om T <150035512+Om-Takbhate@users.noreply.github.com>
2024-10-11 18:26:48 +0530

    Update app.js
    included the code connecting to mongodb cloud cluster and other code

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express')
+require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose')
 const {Listing} = require('./models/listing.model.js')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
+
+console.log(process.env.MONGODB_URI);
+require('dotenv').config(); // Load environment variables from .env file
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 
 app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'/views'))
+app.set('/views',path.join(__dirname,'/views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.engine('ejs',ejsMate)
 app.use(express.static(path.join(__dirname,'/public')))
 
 app.get('/',(req,res)=>{
-    res.redirect('/listings');
+    res.send(`Hi I am root`)
 })
 
 
@@ -24,19 +35,19 @@ app.get('/',(req,res)=>{
 //display all listings route
 app.get('/listings',async(req,res)=>{
     let allListings = await Listing.find({})
-    res.render('listing/index.ejs',{allListings})
+    res.render('listings/index.ejs',{allListings})
 })
 
 //render new.ejs form route
 app.get('/listings/new',(req,res)=>{
-    res.render('listing/new.ejs')
+    res.render('listings/new.ejs')
 })
 
 //show single listing route / read / get route
 app.get('/listings/:id',async (req,res)=>{
     let {id} = req.params
     let listing = await Listing.findById(id)
-    res.render('listing/show.ejs',{listing})
+    res.render('listings/show.ejs',{listing})
 })
 
 //create listing or post route
@@ -50,7 +61,7 @@ app.post('/listings',async(req,res)=>{
 app.get('/listings/:id/edit',async (req,res)=>{
     let {id} = req.params
     let listing = await Listing.findById(id)
-    res.render('listing/edit.ejs',{listing})
+    res.render('listings/edit.ejs',{listing})
 })
 
 //update route
