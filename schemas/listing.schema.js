a028f803d066046e99104bc7a15e5b2892ded3c7
Om T <150035512+Om-Takbhate@users.noreply.github.com>
2024-10-11 18:28:39 +0530

    Update listing.schema.js
    removed the code to connect to mongdb database available on local machine , since the mongdb database will be connected to mongdb clustere cloud

--- schemas/listing.schema.js
+++ schemas/listing.schema.js
@@ -1,44 +1,17 @@
 const mongoose = require('mongoose')
 const {Schema} = mongoose
 
-    main()
-        .then(()=>{
-            console.log(`DB connection successful`)
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
-
-    async function main(){
-        mongoose.connect('mongodb://127.0.0.1:27017/WanderLust2')
-    }
-
-
 const listingSchema = new Schema({
-    title:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String
-    },
+    title:{type:String , required:true},
+    description : {type:String},
     image : {
         type:String,
-        default: 'https://dbsamoa.ws/wp-content/uploads/2022/08/Not-available.jpg',
-        set : (link) => (link==='') ? 'https://dbsamoa.ws/wp-content/uploads/2022/08/Not-available.jpg' : link
+        default: "https://images.unsplash.com/photo-1716807335095-8948ce6ab482?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v==="" ? "https://images.unsplash.com/photo-1716807335095-8948ce6ab482?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
     },
-    price:{
-        type:Number
-    },
-    location:{
-        type:String
-    },
-    country:{
-        type:String,
-        set:(name)=>name.toLowerCase(),
-    }
+    price : {type:String},
+    location:{type:String},
+    country : {type:String}
 })
 
-module.exports = {
-    listingSchema
-}
+module.exports = {listingSchema}
