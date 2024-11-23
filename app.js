2eb7565533e62e281122a33bbd35beefe37f9e0c
Om T <150035512+Om-Takbhate@users.noreply.github.com>
2024-11-23 22:13:57 +0530

    Update app.js
    added compression module
    implementated pagination

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const {Listing} = require('./models/listing.model.js')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const compression = require('compression')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000;
@@ -18,13 +19,13 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 
-
-app.set('view engine','ejs')
-app.set('/views',path.join(__dirname,'/views'))
+app.use(compression())
+app.use(express.static(path.join(__dirname,'/public')))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+app.set('view engine','ejs')
+app.set('/views',path.join(__dirname,'/views'))
 app.engine('ejs',ejsMate)
-app.use(express.static(path.join(__dirname,'/public')))
 
 app.get('/',(req,res)=>{
     res.redirect('/listings')
@@ -34,7 +35,11 @@ app.get('/',(req,res)=>{
 
 //display all listings route
 app.get('/listings',async(req,res)=>{
-    let allListings = await Listing.find({})
+    const page = parseInt(req.query.page) || 1;
+    const limit = 12; // Number of listings per page
+    const skip = (page - 1) * limit;
+
+    let allListings = await Listing.find({}).skip(skip).limit(limit).lean();
     res.render('listings/index.ejs',{allListings})
 })
 
@@ -46,7 +51,7 @@ app.get('/listings/new',(req,res)=>{
 //show single listing route / read / get route
 app.get('/listings/:id',async (req,res)=>{
     let {id} = req.params
-    let listing = await Listing.findById(id)
+    let listing = await Listing.findById(id).lean()
     res.render('listings/show.ejs',{listing})
 })
 
@@ -60,7 +65,7 @@ app.post('/listings',async(req,res)=>{
 //render new.ejs route
 app.get('/listings/:id/edit',async (req,res)=>{
     let {id} = req.params
-    let listing = await Listing.findById(id)
+    let listing = await Listing.findById(id).lean()
     res.render('listings/edit.ejs',{listing})
 })
 
